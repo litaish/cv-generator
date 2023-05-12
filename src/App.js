c9abea2c244792769cf5d8cc922933a07c9ee61b
Lita Strode <l.strode000@gmail.com>
2023-05-12 19:41:07 +0300

    Convert App.js with state to functional component

--- src/App.js
+++ src/App.js
@@ -2,46 +2,31 @@ import AppStyles from "./styles/App.module.css";
 import Header from "./components/Header";
 import DataForm from "./components/DataForm";
 import CV from "./components/CV";
-import { Component } from "react";
+import { useState } from "react";
 
-class App extends Component {
-  constructor(props) {
-    super(props)
+const App = () => {
+  const [formData, setFormData] = useState({});
 
-    this.state = {
-      formData: {},
-    }
-
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleSubmit(e, data) {
+  const handleSubmit = (e, data) => {
+    // Prevent form from submitting on submit
     e.preventDefault();
-
     // Set state to passed in form data (form state)
-    this.setState({
-      formData: data
-    })
-
+    setFormData(data);
     // Reset all form fields on submit
     e.target.reset();
   }
 
-  render() {
-    const { formData } = this.state;
-
-    return (
-      <div className={AppStyles.container}>
-        <Header />
-        <DataForm submitForm={this.handleSubmit} />
-        <CV general={formData.general}
-          skills={formData.skills}
-          educationSections={formData.educationSections}
-          experienceSections={formData.experienceSections}
-        />
-      </div>
-    )
-  }
+  return (
+    <div className={AppStyles.container}>
+      <Header />
+      <DataForm submitForm={handleSubmit} />
+      <CV general={formData.general}
+        skills={formData.skills}
+        educationSections={formData.educationSections}
+        experienceSections={formData.experienceSections}
+      />
+    </div>
+  )
 }
 
 export default App;
